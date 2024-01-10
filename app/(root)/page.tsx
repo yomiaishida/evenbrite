3e9e27a9f23fe3c1892fe693e38844084b1dae3b
Abayomi Aishida <yomicorperate@gmail.com>
2024-01-10 13:45:38 +0100

    Assets and root layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">Eventbrite</h1>
+    </main>
+  );
+}
